b272e6f76ddd8aeaa7bf3acb2258b30ed49e2b5f
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-04-15 11:30:37 +0200

    added attack function, healthBar update function

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
+function getRandomValue(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const app = Vue.createApp({
     data() {
         return {
-            
+            playerHealth: 100,
+            monsterHealth: 100
+        };
+    },
+    computed: {
+        monsterBar(){
+            return {width: this.monsterHealth + '%'}
+        },
+        playerBar(){
+            return {width: this.playerHealth + '%'}
+        }
+    },
+    methods: {
+        attackMonster(){
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
         }
     }
 });
