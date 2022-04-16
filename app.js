c64acdc5f0589054852b83b449ae97f8f408b7df
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-04-16 11:45:13 +0200

    added BattleLog function

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
             monsterHealth: 100,
             currentRound: 0,
             winner: null,
+            logMessages: [],
         };
     },
     computed: {
@@ -51,16 +52,19 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
+            this.addLog("player", "attack", attackValue);
             this.attackPlayer();
         },
         attackPlayer(){
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+            this.addLog("monster", "attack", attackValue);
         },
         specialAttack(){
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.addLog("player", "special-attack", attackValue);
             this.attackPlayer();
         },
         healPlayer(){
@@ -72,6 +76,7 @@ const app = Vue.createApp({
             else{
                 this.playerHealth += healValue;
             }
+            this.addLog("player", "heals", healValue);
             this.attackPlayer();
         },
         startNewGame(){
@@ -79,9 +84,17 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.currentRound = 0;
             this.winner = null;
+            this.logMessages = [];
         },
         surrender(){
             this.winner = "monster";
+        },
+        addLog(who, action, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: action,
+                actionValue: value
+            });
         }
     }
 });
